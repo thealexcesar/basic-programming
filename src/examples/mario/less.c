f6458247467d95cb6b839ad0e116ebf19ce34586
Alex Cesar <thealexcesar@gmail.com>
2025-03-07 19:05:27 -0300

    feature: examples mario

---
+++ src/examples/mario/less.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "get_height.h"
+
+void pyramid(int height);
+
+int main(void) {
+    int height = get_height();
+    pyramid(height);
+}
+
+void pyramid(int height) {
+    for (int i = 1; i <= height; i++) {
+        for (int j = 0; j <= height - i; j++) {
+            printf(" ");
+        }
+
+        for (int j = 0; j < i; j++) {
+            printf("#");
+        }
+        puts("");
+    } 
+}
