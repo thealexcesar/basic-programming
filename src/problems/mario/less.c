5ec57de271e43e6831321c34d26c5bb65e1efc6f
Alex Cesar <thealexcesar@gmail.com>
2025-03-09 14:59:09 -0300

    feat: problem mario cs50

--- src/problems/mario/less.c
+++ src/problems/mario/less.c
@@ -10,7 +10,7 @@ int main(void) {
 
 void pyramid(int height) {
     for (int i = 1; i <= height; i++) {
-        for (int j = 0; j <= height - i; j++) {
+        for (int j = 0; j < height - i; j++) {
             printf(" ");
         }
 
